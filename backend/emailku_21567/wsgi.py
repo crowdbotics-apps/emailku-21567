21836240ba93d60487893ef17b8ba7e1df38adfe
Crowdbotics <team@crowdbotics.com>
2020-10-16 09:40:39 +0000

    Update models

--- backend/emailku_21567/wsgi.py
+++ backend/emailku_21567/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emailku_21567.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "emailku_21567.settings")
 
 application = get_wsgi_application()
